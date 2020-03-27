55296c1cffbcd00616ca40eb7b085523904773aa
Ian Cervone <ian@Ians-MacBook-Pro.local>
2020-03-27 10:49:50 -0400

    added and orgnaized files and folders

---
+++ AzureOverall/AzureOverall/Views/CartTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CartTableViewCell.swift
+//  AzureOverall
+//
+//  Created by Ian Cervone on 3/27/20.
+//  Copyright © 2020 Ian Cervone. All rights reserved.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
